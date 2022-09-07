d8795f7c9c6d19bc14fca2401de9db76ae7fed9a
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-09-08 02:50:08 +0400

    MovementFollower v2

--- Assets/Scripts/Other/MovementFollower.cs
+++ Assets/Scripts/Other/MovementFollower.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class MovementFollower : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+       [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
 
     [SerializeField] private float smoothSpeed = 0.1f;
 
     private Vector3 velosity = Vector3.zero;
 
-    private void LateUpdate()
-    {
+    private void Update(){
         SmoothMovement();
     }
 
-    private void SmoothMovement()
-    {
+    private void SmoothMovement(){
         Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velosity, smoothSpeed);
     }
